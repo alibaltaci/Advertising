1c8d7ebcbe95f9c39a2deaa9ee215b991d3f1070
alibaltaci <alibaltaci13@gmail.com>
2021-02-14 21:23:20 +0300

    MULTIPLE LINEAR REGRESSION

--- Advertising_Linear_Models.py
+++ Advertising_Linear_Models.py
@@ -1,5 +1,6 @@
 # Advertising - Simple Linear Models
 
+# Import libraries
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -11,3 +12,93 @@ from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import GridSearchCV
 import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+os.getcwd()
+
+# Load the datasets
+df = pd.read_csv(r"C:\Users\TOSHIBA\Desktop\Advertising\Advertising.csv", index_col=0)
+df.head()
+
+
+def load_advertising():
+    dataframe = pd.read_csv(r"C:\Users\TOSHIBA\Desktop\Advertising\Advertising.csv", index_col=0)
+    return dataframe
+
+df = load_advertising()
+df.head()
+
+# General Overview
+df.describe().T
+df.info()
+
+sns.jointplot(x="TV", y="sales", data=df, kind="reg")
+plt.show()
+
+X = df[["TV"]]
+X.head()
+y = df[["sales"]]
+
+# MODEL
+reg_model = LinearRegression()
+reg_model.fit(X, y)
+dir(reg_model)
+# y_hat = b0 + b1X
+
+reg_model.intercept_
+reg_model.coef_
+reg_model.score(X, y)
+
+# Predictive
+# y_hat = 7.032 + 0.047*TV
+7.032 + 0.047*160
+
+g = sns.regplot(x="TV", y="sales", data=df, scatter_kws={'color': 'b', 's': 9})
+g.set_title("Model equation: Sales = 7.03 + TV*0.05")
+g.set_ylabel("Number of sales")
+g.set_xlabel("TV Expenses")
+plt.xlim(-10, 310)
+plt.ylim(bottom=0)
+plt.show()
+
+reg_model.intercept_ + reg_model.coef_*500
+reg_model.predict([[165]])
+new_data = [[5], [15], [30]]
+reg_model.predict(new_data)
+
+# MULTIPLE LINEAR REGRESSION
+df = load_advertising()
+df.head()
+
+X = df.drop('sales', axis=1)
+y = df[["sales"]]
+
+# MODEL
+reg_model = LinearRegression()
+reg_model.fit(X, y)
+reg_model.intercept_
+reg_model.coef_
+
+# Sales = 2.94 + TV * 0.04 + radio * 0.19 - newspaper * 0.001
+
+# 30 unit TV, 10 unit radio, 40 unit newspaper
+
+2.94 + 30 * 0.04 + 10 * 0.19 - 40 * 0.001
+
+new_data = [[30], [10], [40]]
+new_data = pd.DataFrame(new_data).T
+reg_model.predict(new_data)
+y.head()
+
+# Evaluating Prediction Success
+reg_model.predict(X)
+y_pred = reg_model.predict(X)
+y_pred[0:5]
+y.head()
+
+mse = mean_squared_error(y, y_pred)
+rmse = np.sqrt(mse)
+df.describe()
+
+
+
